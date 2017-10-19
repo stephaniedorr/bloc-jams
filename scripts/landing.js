bfcca8e9abf577c7dd47a2d3cfa52d220761715a
Stephanie Dorr <stephaniedorr@Stephanies-iMac.local>
2017-10-19 15:52:13 -0400

    Assignment 22 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,28 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
  
-     var points = document.getElementsByClassName('point');
- 
-     var revealPoint = function(index) {
-         points[index].style.opacity = 1;
-         points[index].style.transform = "scaleX(1) translateY(0)";
-         points[index].style.msTransform = "scaleX(1) translateY(0)";
-         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
+ var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
+
+    var animatePoints = function(points)  {
+        forEach(points, revealPoint);
+    };   
+
+
+  window.onload = function() {
+    // Automatically animates the points on a tall screen... 
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+
     
-    for (var i=0; i<points.length; i++) {
-        revealPoint(i);
-    }
- 
- };
-animatePoints();
+     window.addEventListener('scroll', function(event) {
+     if (pointsArray[0].getBoundingClientRect().top <= 500) {
+             animatePoints(pointsArray);   
+         }
+     });
+ }    
+     
