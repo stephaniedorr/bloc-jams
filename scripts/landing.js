7f8f172b4a64ff19133cc52fe1df87e6dea9c6c2
Stephanie Dorr <stephaniedorr@Stephanies-iMac.local>
2017-11-05 10:07:17 -0500

    checkpoint 28 jquery landing completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,28 +1,30 @@
-var pointsArray = document.getElementsByClassName('point');
+
  
- var revealPoint = function(point) {
-    point.style.opacity = 1;
-    point.style.transform = "scaleX(1) translateY(0)";
-    point.style.msTransform = "scaleX(1) translateY(0)";
-    point.style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
+ var animatePoints = function() {
+     var revealPoint = function() {
+         
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+     };
+     $.each($('.point'), revealPoint);
+ };
+ 
 
-    var animatePoints = function(points)  {
-        forEach(points, revealPoint);
-    };   
-
-
-  window.onload = function() {
-    // Automatically animates the points on a tall screen... 
-     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+  $(window).load(function() {
+    if ($(window).height() > 950) {
+         animatePoints();
      }
 
-    
-     window.addEventListener('scroll', function(event) {
-     if (pointsArray[0].getBoundingClientRect().top <= 500) {
-             animatePoints(pointsArray);   
+     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+     $(window).scroll(function(event) {
+         if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
          }
      });
- }    
-     
+ });
+
+
+
